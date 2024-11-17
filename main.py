e33a27cdc1d3f8348a6475fbb66f11b4946fcca6
TatyanaPonomareva <tanyfka2010p@gmail.com>
2024-11-17 20:26:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# 1st program
+
+print((9 ** 0.5) * 5)
+
+# 2nd program
+
+print(9.99 > 9.98, 1000 != 1000.1)
+
+# 3rd program
+
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+# 4tf program
+
+stroka = '123.456'
+number = float(stroka)
+number = number * 10
+number = int(number)    # number = 1234
+result = number % 10    # result = 4
+print(result)
